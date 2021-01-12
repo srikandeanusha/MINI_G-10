4d435faa86f17810db0f25d5f9fc50b6dee08998
kadudulasahithi <76682063+kadudulasahithi@users.noreply.github.com>
2021-01-12 15:00:54 +0530

    Add files via upload

---
+++ js/editprofile.js
@@ -0,0 +1,5 @@
+function back() {
+    window.history.back();
+  }
+  
+  
---
+++ js/user1.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $('[data-toggle="offcanvas"]').click(function(){
+        $("#navigation").toggleClass("hidden-xs");
+    });
+ });
+ 
+ function f1()
+ {
+      document.getElementById("book").innerHTML;
+      location.replace("http://127.0.0.1:5500/html/html5.html")
+ }
+function f2()
+  {
+     document.getElementById("book").innerHTML;
+     location.replace("http://127.0.0.1:5500/html/css3.html")
+ }
+function f3()
+ {
+     document.getElementById("book").innerHTML;
+     location.replace("http://127.0.0.1:5500/html/javascript.html")
+ }
+function f4()
+ {
+     document.getElementById("book").innerHTML;
+     location.replace("http://127.0.0.1:5500/html/jquery.html")
+ }
+function f5()
+ {
+     document.getElementById("book").innerHTML;
+     location.replace("http://127.0.0.1:5500/html/bootstrap.html")
+ } 
+ function f6()
+ {
+     document.getElementById("score").innerHTML="Your Score displays here...";
+ }
+ function f7()
+ {
+     document.getElementById("graph").innerHTML="Graph...";
+ }
+ function f8()
+ {
+     document.getElementById("perform").innerHTML="Performance...";
+ }    
+
+ function myFunction() {
+    document.getElementById("book").innerHTML="Start HTML5 Quiz"
+
+    location.replace("https://www.w3schools.com")
+  }
